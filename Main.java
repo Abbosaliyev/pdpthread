881dcc3fae18084e5ae3a1226dba0ae863152615
aminov-nurmuhammad <only.pubgm.player@gmai.com>
2024-02-05 16:58:29 +0500

    Nurmuhammad

--- Main.java
+++ Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
     public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        ExecutorService service = Executors.newFixedThreadPool(3);
 
-        System.out.println("Hi");
+        System.out.print("Enter something: ");
+        String a = new Scanner(System.in).nextLine();
 
+        service.execute(()-> {
+            lock.lock();
+            intA.isInteger(a);
+            lock.unlock();
+        });
+        service.execute(()-> {
+            lock.lock();
+            stringA.isString(a);
+            lock.unlock();
+        });
+        service.execute(()-> {
+            lock.lock();
+            charA.isChar(a);
+            lock.unlock();
+        });
+        service.shutdown();
     }
 }
