237225f95818863cd239d015be35d4db53eae7fe
salmon0606 <salmonabdusattorov155@gmai.com>
2024-02-05 17:00:19 +0500

    Salmon

---
+++ out/src/Main.java
@@ -0,0 +1,50 @@
+import GenerateRandomString.GenerateRandomString;
+import ReturnEnteredString.ReturnEnteredString;
+import ReverseString.ReverceEnteredString;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        while (true){
+            menu();
+            switch (new Scanner(System.in).nextInt()){
+                case 1:
+                    lock.lock();
+                    ReturnEnteredString.returnEnteredString();
+                    lock.unlock();
+                    break;
+
+                case 2:
+                    lock.lock();
+                    ReverceEnteredString.reverseString();
+                    lock.unlock();
+                    break;
+
+                case 3:
+                    lock.lock();
+                    GenerateRandomString.generateRandomString(5);
+                    lock.unlock();
+            }
+        }
+
+    }
+
+    private static void menu() {
+        System.out.println("Choose one of the actions: ");
+        System.out.println("1-return entered text");
+        System.out.println("2-return reverse entered text");
+        System.out.println("3-generate random string");
+    }
+}
