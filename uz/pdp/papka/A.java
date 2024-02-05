ef0150bb2a8f8d4ed1ca5cd19d84a1463aa26b00
lenovo <fazliddinnnosirjovov07@gnail.com>
2024-02-05 17:25:21 +0500

    Thread

--- uz/pdp/papka/A.java
+++ uz/pdp/papka/A.java
@@ -3,14 +3,11 @@ package uz.pdp.papka;
 public class A implements Runnable {
     @Override
     public  void run() {
-        Thread thread=new Thread(()->{
             synchronized (this){
                 System.out.println("Hello word !");
                 int i=20;
                 int n=60;
                 System.out.println("sum: "+(i+n));
             }
-        });
-        thread.start();
     }
 }
--- uz/pdp/papka/C.java
+++ uz/pdp/papka/C.java
@@ -3,6 +3,11 @@ package uz.pdp.papka;
 public class C implements Runnable{
     @Override
     public void run() {
-
+        synchronized (this){
+            System.out.println("Hello word !");
+            int i=20;
+            int n=60;
+            System.out.println("sum: "+(i+n));
+        }
     }
 }
