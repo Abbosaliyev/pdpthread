db6b32adfcb3c997b882b081f1e1ed8fb0ed2e64
samriddin <samariddinkhatamov@gmail.com>
2024-02-05 16:58:49 +0500

    Samariddin

--- Main.java
+++ Main.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+while (true){
+        DisplayMenu();
+        switch (scanner.nextInt()) {
+            case 1 -> {
+                A a = new A();
+                a.run();
+            }
+            case 2 -> {
+                B b = new B();
+                b.run();
+            }
+            case 3 -> {
+                C c = new C();
+                c.run();
+            }
+            case 4 -> {
+                D d = new D();
+                d.run();
+            }
+         }
+       }
+    }
 
-        System.out.println("Hi");
-
+    private static void DisplayMenu() {
+        System.out.println("Choose");
+        System.out.println("1.Class A");
+        System.out.println("2.Class B");
+        System.out.println("3.Class C");
+        System.out.println("4.Class D");
+        System.out.println("    ");
+        System.out.println("    ");
     }
 }
