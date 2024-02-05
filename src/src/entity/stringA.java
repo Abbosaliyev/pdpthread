5a969a7bfe817d92e88af1aaf0335ae883d28ac2
aminov-nurmuhammad <only.pubgm.player@gmai.com>
2024-02-05 17:40:06 +0500

    Nurmuhammad

--- src/src/entity/stringA.java
+++ src/src/entity/stringA.java
@@ -2,7 +2,7 @@ package entity;
 
 public class stringA {
     public static void isString(String a) {
-        if(a.length() > 1) {
+        if(!intA.isInt(a) && a.length() > 1) {
             System.out.println("String value: " + a);
         }
     }
